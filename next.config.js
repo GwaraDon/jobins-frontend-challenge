6a7e7507308d7d0ad236b8ba08a54b93967c387d
Binay Maharjan <binay.maharjan@ekbana.info>
2023-06-07 19:21:27 +0545

    Comment the next configuration

--- next.config.js
+++ next.config.js
@@ -5,8 +5,9 @@ const nextConfig = {
     loader: "default",
     path: "/_next/image",
   },
-  output: "export",
-  distDir: "build",
+  // uncomment output and distDir to see the build
+  // output: "export",
+  // distDir: "build",
 };
 
 module.exports = nextConfig;
